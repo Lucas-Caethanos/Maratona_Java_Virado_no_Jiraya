eb602dd753530bd84f25a15a7258ef1f9b26c142
Lucas-Caethanos <lucascaetanocosta@gmail.com>
2024-12-15 16:41:51 -0300

    Aulas de POO - Sobrecarga de Métodos 🥤☕

--- src/E_OrientacaoObjetos/C_SobrecargaMetodos/Dominio/Anime.java
+++ src/E_OrientacaoObjetos/C_SobrecargaMetodos/Dominio/Anime.java
@@ -1,64 +1,76 @@
 package E_OrientacaoObjetos.C_SobrecargaMetodos.Dominio;
 
+// Classe que representa informações sobre um anime
 public class Anime {
-    public String nome;
-    private String tipo;
-    private int episodios;
-    private String genero;
+    // Atributos da classe
+    public String nome;         // Nome do anime
+    private String tipo;        // Tipo do anime (por exemplo, TV, OVA, Filme)
+    private int episodios;      // Número de episódios do anime
+    private String genero;      // Gênero do anime (por exemplo, ação, comédia)
 
+    // Método_ para imprimir as informações do anime
     public void imprime() {
+        // Usa 'this' para acessar os atributos da instância atual
         System.out.println(this.nome);
         System.out.println(this.tipo);
         System.out.println(this.episodios);
         System.out.println(this.genero);
     }
 
+    // Método_ 'init' (inicializador) que configura os atributos 'nome', 'tipo' e 'episodios'
     public void init(String nome, String tipo, int episodios) {
-        this.nome = nome;
-        this.tipo = tipo;
-        this.episodios = episodios;
+        this.nome = nome;           // Inicializa o nome do anime
+        this.tipo = tipo;           // Inicializa o tipo do anime
+        this.episodios = episodios; // Inicializa o número de episódios
     }
 
-    //Método sobrescrito
+    // Sobrecarga do método_ 'init', incluindo o atributo 'genero'
     public void init(String nome, String tipo, int episodios, String genero) {
-        //Chamando um método antigo no método sobrescrito
+        // Reutiliza o método_ 'init' com 3 parâmetros para evitar duplicação de código
         this.init(nome, tipo, episodios);
 
-        //this.nome = nome;
-        //this.tipo = tipo;
-        //this.episodios = episodios;
+        // Configura o atributo 'genero' adicionalmente
         this.genero = genero;
     }
 
+    // Métodos setters e getters para acessar e modificar os atributos de forma controlada
 
+    // Setter para o atributo 'tipo'
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
 
+    // Getter para o atributo 'tipo'
     public String getTipo() {
         return this.tipo;
     }
 
+    // Setter para o atributo 'episodios'
     public void setEpisodios(int episodios) {
         this.episodios = episodios;
     }
 
+    // Getter para o atributo 'episodios'
     public int getEpisodios() {
         return this.episodios;
     }
 
+    // Setter para o atributo 'nome'
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    // Getter para o atributo 'nome'
     public String getNome() {
         return this.nome;
     }
 
+    // Getter para o atributo 'genero'
     public String getGenero() {
         return genero;
     }
 
+    // Setter para o atributo 'genero'
     public void setGenero(String genero) {
         this.genero = genero;
     }
--- src/E_OrientacaoObjetos/C_SobrecargaMetodos/Test/AnimeTest01.java
+++ src/E_OrientacaoObjetos/C_SobrecargaMetodos/Test/AnimeTest01.java
@@ -1,20 +1,27 @@
 package E_OrientacaoObjetos.C_SobrecargaMetodos.Test;
 
+// Importa a classe Anime do pacote indicado
 import E_OrientacaoObjetos.C_SobrecargaMetodos.Dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
+        // Criação de uma nova instância da classe Anime
         Anime anime = new Anime();
 
-        //Inicializando os atributos por um método único
+        // Inicializando os atributos utilizando o método_ init com 3 parâmetros
         anime.init("Bleach", "TV", 135);
+
+        // Inicializando novamente, agora com 4 parâmetros (sobrecarga do método_ init)
+        // O método_ init com 4 parâmetros chama o init com 3 parâmetros e adiciona o 'gênero'
         anime.init("Bleach", "TV", 135, "Ação");
 
-//        anime.setNome("Bleach");
-//        anime.setTipo("TV");
-//        anime.setEpisodios(135);
+        // Comentário: Este trecho foi substituído pela utilização do método_ init
+        // Inicialização manual por setters (não usada aqui, mas também válida)
+        // anime.setNome("Bleach");
+        // anime.setTipo("TV");
+        // anime.setEpisodios(135);
+
+        // Chama o método_ imprime para exibir as informações do anime
         anime.imprime();
-
-
     }
 }
