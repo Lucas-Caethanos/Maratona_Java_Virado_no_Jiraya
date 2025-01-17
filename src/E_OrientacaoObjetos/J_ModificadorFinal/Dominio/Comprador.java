2183b63648f79185b5b16b11e3ccda4a0543aa5c
Lucas-Caethanos <lucascaetanocosta@gmail.com>
2025-01-17 19:05:22 -0300

    Aulas de POO - Modificador Final 🥤☕

--- src/E_OrientacaoObjetos/J_ModificadorFinal/Dominio/Comprador.java
+++ src/E_OrientacaoObjetos/J_ModificadorFinal/Dominio/Comprador.java
@@ -1,19 +1,25 @@
 package E_OrientacaoObjetos.J_ModificadorFinal.Dominio;
 
+// Classe Comprador que representa uma pessoa que realiza a compra de um carro.
 public class Comprador {
+    // Atributo privado para armazenar o nome do comprador.
     private String Nome;
 
+    // Sobrescrita do método toString() para personalizar a representação textual do objeto.
     @Override
     public String toString() {
+        // Retorna uma string formatada contendo o valor do atributo 'Nome'.
         return "Comprador{" +
                 "Nome='" + Nome + '\'' +
                 '}';
     }
 
+    // Getter para obter o valor do atributo 'Nome'.
     public String getNome() {
         return Nome;
     }
 
+    // Setter para modificar o valor do atributo 'Nome'.
     public void setNome(String nome) {
         Nome = nome;
     }
--- src/E_OrientacaoObjetos/J_ModificadorFinal/Test/CarroTest01.java
+++ src/E_OrientacaoObjetos/J_ModificadorFinal/Test/CarroTest01.java
@@ -1,17 +1,27 @@
 package E_OrientacaoObjetos.J_ModificadorFinal.Test;
 
+// Importa as classes necessárias para o teste.
 import E_OrientacaoObjetos.J_ModificadorFinal.Dominio.Comprador;
 import E_OrientacaoObjetos.J_ModificadorFinal.Dominio.Carro;
 
+// Classe de teste para a classe Carro.
 public class CarroTest01 {
     public static void main(String[] args) {
-       Carro carro = new Carro();
+        // Cria um objeto da classe Carro.
+        Carro carro = new Carro();
 
+        // Cria um objeto da classe Comprador.
         Comprador comprador2 = new Comprador();
+
+        // Define o nome do comprador associado ao carro.
+        // Apesar de 'COMPRADOR' ser um atributo 'final', é possível alterar os atributos do objeto.
         carro.COMPRADOR.setNome("Ichigo");
+
+        // Imprime as informações do comprador associado ao carro.
+        // O método toString() da classe Comprador será chamado automaticamente.
         System.out.println(carro.COMPRADOR);
 
+        // Imprime o valor da constante estática VELOCIDADE_LIMITE.
         System.out.println(Carro.VELOCIDADE_LIMITE);
-
     }
 }
