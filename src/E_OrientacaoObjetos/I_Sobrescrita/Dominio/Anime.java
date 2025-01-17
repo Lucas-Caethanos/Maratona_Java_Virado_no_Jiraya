0f66a67dbbba8dfa25c35849658a9a9dd99bebc6
Lucas-Caethanos <lucascaetanocosta@gmail.com>
2025-01-17 18:49:11 -0300

    Aulas de POO - Sobrescrita 🥤☕

--- src/E_OrientacaoObjetos/I_Sobrescrita/Dominio/Anime.java
+++ src/E_OrientacaoObjetos/I_Sobrescrita/Dominio/Anime.java
@@ -1,23 +1,31 @@
 package E_OrientacaoObjetos.I_Sobrescrita.Dominio;
 
+// Classe Anime que representa um objeto com um atributo 'nome'.
 public class Anime {
+
+    // Atributo privado para armazenar o nome do anime.
     private String nome;
 
+    // Construtor que inicializa o atributo 'nome' com o valor fornecido.
     public Anime(String nome) {
         this.nome = nome;
     }
 
+    // Sobrescrita do método_ toString() para personalizar como o objeto é representado como uma String.
     @Override
     public String toString() {
+        // Retorna uma representação personalizada do objeto.
         return "Anime{" +
                 "nome='" + nome + '\'' +
                 '}';
     }
 
+    // Getter para obter o valor do atributo 'nome'.
     public String getNome() {
         return nome;
     }
 
+    // Setter para modificar o valor do atributo 'nome'.
     public void setNome(String nome) {
         this.nome = nome;
     }
--- src/E_OrientacaoObjetos/I_Sobrescrita/Test/AnimeTest01.java
+++ src/E_OrientacaoObjetos/I_Sobrescrita/Test/AnimeTest01.java
@@ -1,13 +1,16 @@
 package E_OrientacaoObjetos.I_Sobrescrita.Test;
 
+// Importa a classe Anime do pacote apropriado.
 import E_OrientacaoObjetos.I_Sobrescrita.Dominio.Anime;
 
+// Classe de teste para verificar o funcionamento da classe Anime.
 public class AnimeTest01 {
     public static void main(String[] args) {
+        // Cria um objeto da classe Anime e inicializa o atributo 'nome' com "Bleach".
         Anime anime = new Anime("Bleach");
 
+        // Imprime o objeto utilizando o método_ toString().
+        // O método_ toString() foi sobrescrito na classe Anime, então, ao invés de imprimir algo como "E_OrientacaoObjetos.I_Sobrescrita.Dominio.Anime@<hashcode>", será exibida uma string personalizada no formato definido: "Anime{nome='Bleach'}".
         System.out.println(anime);
-
     }
 }
-
